7acf4f2f01f17e178eb0c2b5fc21337fee411b3a
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2024-04-01 12:14:23 +0200

    feat: Attempt to add MQTT support

--- conf/asgi.py
+++ conf/asgi.py
@@ -1,16 +1,14 @@
-"""
-ASGI config for conf project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
+import django
+from channels.routing import ProtocolTypeRouter
+from sagor.consumers import SagorMqttConsumer
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 
-application = get_asgi_application()
+django.setup()
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'mqtt': SagorMqttConsumer.as_asgi(),
+})
