5a79148250887ecf2e1c857a3697f16ebe567a97
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2024-04-27 22:44:20 +0300

    feat: Use dj_database_url

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # Backend for Sagor
+# Instruction for development
+
+- Set up database
+```
+postgres=# CREATE DATABASE "sagor-database";
+CREATE DATABASE
+postgres=# CREATE USER sagor WITH SUPERUSER PASSWORD 'sagor';
+CREATE ROLE
+postgres=# GRANT ALL PRIVILEGES ON DATABASE "sagor-database" TO sagor;
+GRANT
+postgres=# 
+```
+
+- Copy the environment variables template to your .env file
+``cp env.template .env``
 
 # BRAINDUMP
 I'm thinking on management commands that are run by crons.
--- conf/settings/base.py
+++ conf/settings/base.py
@@ -2,6 +2,12 @@ import os
 from pathlib import Path
 import datetime
 
+import dj_database_url
+from dotenv import load_dotenv
+
+# load .env
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,12 +62,11 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
