09af81eff09c22e32179ebfe6bd4ae563202e955
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2024-04-27 17:02:02 +0300

    Revert "Fixing some model fields"
    This reverts commit 63a05ba5344c918725de3adb8bf134a4424b8296.

--- sagor/models.py
+++ sagor/models.py
@@ -1,44 +1,45 @@
 from django.db import models
 
-
 class Farm(models.Model):
     name = models.CharField(max_length=250)
 
 
 class Gateway(models.Model):
     class Status(models.TextChoices):
-        OK = "OK", "Ok"
-        ERROR = "ERROR", "Error"
+        OK = 'OK', 'Ok'
+        ERROR = 'ERROR', 'Error'
+
 
     broker_url = models.URLField()
-    ip = models.GenericIPAddressField()
+    ip = models.IPAddressField()
     status = models.CharField(
-        max_length=250,
+        max_length = 250,
         choices=Status.choices,
         default=Status.OK,
     )
     mac_address = models.CharField(max_length=250)
 
     farm = models.ForeignKey(
-        "Farm",
+        'Farm',
         on_delete=models.CASCADE,
-        related_name="gateways",
+        related_name='gateways',
     )
 
 
 class Tanks(models.Model):
     class FishType(models.TextChoices):
-        TILAPIA = "TILAPIA", "Tilapia"
+        TILAPIA = 'TILAPIA', 'Tilapia'
+    
 
     class Status(models.TextChoices):
-        """
+        '''
         You have to think before extending this enum.
         For example, a tank can have overfed fish AND
         unhealthy water. There are a lot of indep statuses.
-        """
+        '''
+        HEALTHY = 'HEALTHY', 'Healthy'
+        UNHEALTHY = 'UNHEALTHY', 'Unhealthy'
 
-        HEALTHY = "HEALTHY", "Healthy"
-        UNHEALTHY = "UNHEALTHY", "Unhealthy"
 
     fish_type = models.CharField(
         max_length=250,
@@ -52,89 +53,100 @@ class Tanks(models.Model):
     )
 
     gateway = models.ForeignKey(
-        "Gateway",
+        'Gateway',
         # consider changing to PROTECT
         on_delete=models.SET_NULL,
-        related_name="tanks",
-        null=True
+        related_name='tanks',
     )
 
 
 class Package(models.Model):
     class Status(models.TextChoices):
-        OK = "OK", "Ok"
-        ERROR = "ERROR", "Error"
+        OK = 'OK', 'Ok'
+        ERROR = 'ERROR', 'Error'
 
-    status = models.CharField(max_length=250, choices=Status.choices, default=Status.OK)
+
+    status = models.CharField(
+        max_length=250,
+        choices=Status.choices,
+        default=Status.OK
+    )
     last_checked_at = models.DateTimeField()
 
     tank = models.ForeignKey(
-        "Package",
+        'Package',
         on_delete=models.SET_NULL,
-        related_name="packages",
-        null=True
+        related_name='packages',
     )
-
 
 class Pump(models.Model):
     class Status(models.TextChoices):
-        ACTIVE = "ACTIVE", "Active"
-        INACTIVE = (
-            "INACTIVE",
-            "Inactive",
-        )
-        ERROR = "ERROR", "Error"
+        ACTIVE = 'ACTIVE', 'Active'
+        INACTIVE = 'INACTIVE', 'Inactive',
+        ERROR = 'ERROR', 'Error'
+
 
     status = models.CharField(
-        max_length=250, choices=Status.choices, default=Status.INACTIVE
+        max_length=250,
+        choices=Status.choices,
+        default=Status.INACTIVE
     )
 
     tank = models.ForeignKey(
-        "Tanks",
+        'Tank',
         on_delete=models.CASCADE,
-        related_name="pumps",
+        related_name='pumps',
     )
 
 
 class PumpedFood(models.Model):
     class Status(models.TextChoices):
-        OK = "OK", "Ok"
-        ERROR = "ERROR", "Error"
+        OK = 'OK', 'Ok'
+        ERROR = 'ERROR', 'Error'
 
-    quantity = models.DecimalField(max_digits=5, decimal_places=2)
-    status = models.CharField(max_length=250, choices=Status.choices, default=Status.OK)
+    
+    quantity = models.DecimalField()
+    status = models.CharField(
+        max_length=250,
+        choices=Status.choices,
+        default=Status.OK
+    )
 
     pump = models.ForeignKey(
-        "Pump",
+        'Pump',
         on_delete=models.CASCADE,
-        related_name="pumped_food",
+        related_name='pumped_food',
     )
 
 
 class BaseSensor(models.Model):
     class ReadingStatus(models.TextChoices):
-        SANE = "SANE", "Sane"
-        NOT_SANE = "NOT_SANE", "Not sane"
+        SANE = 'SANE', 'Sane'
+        NOT_SANE = 'NOT_SANE', 'Not sane'
+
 
     reading_status = models.CharField(
         max_length=250,
         choices=ReadingStatus.choices,
         default=ReadingStatus.SANE,
     )
-    run_every = models.DurationField()
+    run_every = models.DecimalField()
 
     package = models.ForeignKey(
-        "Package", on_delete=models.CASCADE, related_name="sensors"
+        'Package',
+        on_delete=models.CASCADE,
+        related_name='sensors'
     )
 
-
 class PHSensor(BaseSensor):
-    value = models.DecimalField(max_digits=5, decimal_places=2)
+    value = models.DecimalField()
 
 
 class TempratureSensor(BaseSensor):
-    value = models.DecimalField(max_digits=5, decimal_places=2)
+    value = models.DecimalField()
 
 
 class CameraSensor(BaseSensor):
     value = models.BinaryField()
+
+
