78c3e88e3fa8fbe7efeecb2ccf58052f5aa8ef41
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2024-04-27 23:17:17 +0300

    fix: Get SECRET_KEY from env in base.py

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -11,6 +11,8 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 ALLOWED_HOSTS = []
 
 # Application definition
