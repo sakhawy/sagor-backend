0bd5975fdc7cb9cb69a0274adac6f42948fde593
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2024-04-30 00:47:43 +0300

    feat: Add ModelViewSets with browsable API

---
+++ sagor/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from sagor import views
+
+router = routers.DefaultRouter()
+router.register(r'farms', views.FarmViewSet)
+router.register(r'gateways', views.GatewayViewSet)
+router.register(r'tanks', views.TankViewSet)
+router.register(r'packages', views.PackageViewSet)
+router.register(r'ph_sensor_readings', views.PHSensorReadingViewSet)
+router.register(r'temprature_sensor_readings', views.TempratureSensorReadingViewSet)
+router.register(r'camera_sensor_readings', views.CameraSensorReadingViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- sagor/views.py
+++ sagor/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from sagor import models
+from sagor import serializers
+
+
+class FarmViewSet(ModelViewSet):
+    queryset = models.Farm.objects.all()
+    serializer_class = serializers.FarmSerializer
+
+
+class GatewayViewSet(ModelViewSet):
+    queryset = models.Gateway.objects.all()
+    serializer_class = serializers.GatewaySerializer
+
+
+class TankViewSet(ModelViewSet):
+    queryset = models.Tank.objects.all()
+    serializer_class = serializers.TankSerializer
+
+
+class PackageViewSet(ModelViewSet):
+    queryset = models.Package.objects.all()
+    serializer_class = serializers.PackageSerializer
+
+
+class PHSensorReadingViewSet(ModelViewSet):
+    queryset = models.PHSensorReading.objects.all()
+    serializer_class = serializers.PHSensorReadingSerializer
+
+
+class TempratureSensorReadingViewSet(ModelViewSet):
+    queryset = models.TempratureSensorReading.objects.all()
+    serializer_class = serializers.TempratureSensorReadingSerializer
+
+
+class CameraSensorReadingViewSet(ModelViewSet):
+    queryset = models.CameraSensorReading.objects.all()
+    serializer_class = serializers.CameraSensorReadingSerializer
