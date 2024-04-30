0d83a1f1c615c547fd8bd0cb0d959655e7ccf389
Mohammad Alsakhawy <mohammadalsakhawy@gmail.com>
2024-04-30 23:27:38 +0300

    fix: Disable authentication temporarily

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -116,17 +116,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-REST_FRAMEWORK = {
-    
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-
-}
-
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=15),
--- conf/urls.py
+++ conf/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/auth/', include('sagor_auth.urls')),
+    # path('api/v1/auth/', include('sagor_auth.urls')),
     path('api/v1/', include('sagor.urls')),
 
     # drf spectacular urls
